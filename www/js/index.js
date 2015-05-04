f8e378dea6dbb3dac40aa8f0fa0f9db4cd36aec1
drose379 <dylanrose60@gmail.com>
2015-05-03 21:54:18 -0400

    syntax error in previous commit

--- www/js/index.js
+++ www/js/index.js
@@ -27,7 +27,7 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
 
     buttonClicked: function() {
         alert("Working");
